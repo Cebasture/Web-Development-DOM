0a0b3eff15afbae3d1d8e0b2f0b742bbffe87ed0
kaustav-karan <kaustavkaran2015@gmail.com>
2024-07-05 00:01:27 +0530

    big fixes

--- JavaScript/DOM/Add a friend/script.js
+++ JavaScript/DOM/Add a friend/script.js
@@ -1,29 +1,27 @@
-var addFrnd = document.querySelector("#add");
+var addFrnd = document.querySelector("#addItem");
 var rmvFrnd = document.querySelector("#remove");
 var istatus = document.querySelector("#status");
 
-addFrnd.addEventListener("mouseenter", function(){
-        addFrnd.style.backgroundColor = "#1947d0"
-    }
-);
+addFrnd.addEventListener("mouseenter", function () {
+  addFrnd.style.backgroundColor = "#1947d0";
+});
 
-addFrnd.addEventListener("mouseleave", function(){
-        addFrnd.style.backgroundColor = "royalblue"
-    }
-);
+addFrnd.addEventListener("mouseleave", function () {
+  addFrnd.style.backgroundColor = "royalblue";
+});
 
-addFrnd.addEventListener("click", function(){
-        istatus.innerHTML = "Friend Request Sent"
-        istatus.style.color = "royalblue"
-        addFrnd.innerHTML = "Unsend"
-        addFrnd.setAttribute("added")
-        // addFrnd.id = "added";
-    }
-);
+addFrnd.addEventListener("click", function () {
+  istatus.innerHTML = "Friend Request Sent";
+  istatus.style.color = "royalblue";
+  addFrnd.innerHTML = "Unsend";
+  addFrnd.classList.add("added");
+  addFrnd.classList.remove("add");
+    // addFrnd.id = "added";
+    console.log("clicked")
+});
 
-var frnd = document.querySelector("added")
+var frnd = document.querySelector("added");
 
-frnd.addEventListener("click", function(){
-        console.log("helo")
-    }
-)
+frnd.addEventListener("click", function () {
+  console.log("helo");
+});
